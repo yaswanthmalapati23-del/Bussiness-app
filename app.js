86cbf10fae28ab7427f9194fd7b94757cbb05842
yaswanthmalapati23-del <yaswanthmalapati23@gmail.com>
2026-03-12 10:07:36 +0530

    Add greetUser function for user interaction

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * Simple function to interact with the user
+ */
+function greetUser() {
+    alert("Thanks for your interest! We will contact you shortly.");
+    console.log("User clicked the Get Started button.");
+}
